44a37fcf659c2dc9b1e33ce770c6cbbba5e01f3b
Kaspian <kacperleaguenine@gmail.com>
2022-07-29 16:47:59 +0200

    Prod ready

--- src/main/java/screw/microsoft/antipopup/AntiPopup.java
+++ src/main/java/screw/microsoft/antipopup/AntiPopup.java
@@ -11,17 +11,20 @@ public final class AntiPopup extends JavaPlugin {
         PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
         PacketEvents.getAPI().getSettings().debug(false).bStats(false).checkForUpdates(false);
         PacketEvents.getAPI().load();
+        getLogger().info("Loaded PacketEvents");
     }
 
     @Override
     public void onEnable() {
         PacketEvents.getAPI().getEventManager().registerListener(new PacketListener());
         PacketEvents.getAPI().init();
+        getLogger().info("Initiated PacketEvents");
     }
 
     @Override
     public void onDisable() {
         PacketEvents.getAPI().terminate();
+        getLogger().info("Disabled PacketEvents");
     }
 
 }
--- src/main/java/screw/microsoft/antipopup/PacketListener.java
+++ src/main/java/screw/microsoft/antipopup/PacketListener.java
@@ -1,18 +1,22 @@
 package screw.microsoft.antipopup;
 
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerServerData;
 
 public class PacketListener extends PacketListenerAbstract {
 
-    public void onPacket(PacketSendEvent event) {
+    public PacketListener() {
+        super(PacketListenerPriority.HIGH);
+    }
+
+    @Override
+    public void onPacketSend(PacketSendEvent event) {
         if (event.getPacketType() == PacketType.Play.Server.SERVER_DATA) {
             WrapperPlayServerServerData serverData = new WrapperPlayServerServerData(event);
             serverData.setEnforceSecureChat(true);
-            System.out.println("packet has been sent");
         }
-
     }
 }
