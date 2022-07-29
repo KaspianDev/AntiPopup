db44aa88f3bcc63c88641c1fa2a80bdd857833ef
Kaspian <kacperleaguenine@gmail.com>
2022-07-29 17:51:18 +0200

    Use PLAYER_CHAT_HEADER packet

--- src/main/java/screw/microsoft/antipopup/PacketEventsListener.java
+++ src/main/java/screw/microsoft/antipopup/PacketEventsListener.java
@@ -2,11 +2,10 @@ package screw.microsoft.antipopup;
 
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
-import com.github.retrooper.packetevents.protocol.chat.message.ChatMessage;
-import com.github.retrooper.packetevents.protocol.chat.message.ChatMessage_v1_19_1;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerChatMessage;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerChatHeader;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerServerData;
 import dev.dejvokep.boostedyaml.YamlDocument;
 
@@ -26,16 +25,17 @@ public class PacketEventsListener extends PacketListenerAbstract {
             WrapperPlayServerServerData serverData = new WrapperPlayServerServerData(event);
             serverData.setEnforceSecureChat(true);
         }
-        if (event.getPacketType() == PacketType.Play.Server.CHAT_MESSAGE
+    }
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() == PacketType.Play.Server.PLAYER_CHAT_HEADER
                 && config.getBoolean("strip-signature").equals(true)) {
-            WrapperPlayServerChatMessage chatMessage = new WrapperPlayServerChatMessage(event);
-            final ChatMessage message = chatMessage.getMessage();
-            if (message instanceof ChatMessage_v1_19_1 v1_19_1) {
-                v1_19_1.setSalt(0L);
-                v1_19_1.setSenderUUID(new UUID(0L, 0L));
-                v1_19_1.setPreviousSignature(null);
-                v1_19_1.setSignature(null);
-            }
+            WrapperPlayServerPlayerChatHeader chatMessage = new WrapperPlayServerPlayerChatHeader(event);
+            chatMessage.setSignature(null);
+            chatMessage.setPreviousSignature(null);
+            chatMessage.setHash(null);
+            chatMessage.setPlayerUUID(new UUID(0L, 0L));
         }
     }
 }
