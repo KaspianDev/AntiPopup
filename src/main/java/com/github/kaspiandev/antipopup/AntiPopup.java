d228bf2a8c88a49ec19e45a4ee672ffa7e66c308
Kaspian <kacperleaguenine@gmail.com>
2022-08-30 14:39:35 +0200

    🔼 Bstats

--- src/main/java/com/github/kaspiandev/antipopup/AntiPopup.java
+++ src/main/java/com/github/kaspiandev/antipopup/AntiPopup.java
@@ -9,6 +9,7 @@ import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
 import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
 import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,6 +26,7 @@ public final class AntiPopup extends JavaPlugin {
 
     static YamlDocument config;
     static Plugin instance;
+    static Metrics metrics;
 
     @Override
     public void onLoad() {
@@ -37,7 +39,6 @@ public final class AntiPopup extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-
         try {
             config = YamlDocument.create(new File(getDataFolder(), "config.yml"),
                     Objects.requireNonNull(getResource("config.yml")),
@@ -51,16 +52,21 @@ public final class AntiPopup extends JavaPlugin {
             throw new RuntimeException(ex);
         }
 
+        if (config.getBoolean("bstats", false)) {
+            metrics = new Metrics(this, 16308);
+            getLogger().info("Loaded optional metrics.");
+        }
+
         PacketEvents.getAPI().getEventManager().registerListener(new PacketEventsListener());
         PacketEvents.getAPI().init();
         getLogger().info("Initiated PacketEvents.");
 
         Objects.requireNonNull(this.getCommand("antipopup")).setExecutor(new CommandRegister());
         getLogger().info("Commands registered.");
-        
+
 
         if (getPluginManager().getPlugin("ViaVersion") != null
-                && PacketEvents.getAPI().getServerManager().getVersion().equals(ServerVersion.V_1_19)) {
+                    && PacketEvents.getAPI().getServerManager().getVersion().equals(ServerVersion.V_1_19)) {
             try {
                 var hookClass = ViaHook.class;
                 hookClass.getConstructor().newInstance();
@@ -92,7 +98,22 @@ public final class AntiPopup extends JavaPlugin {
                     io.printStackTrace();
                 }
             }
-        }, 1);
+            if (config.getBoolean("ask-bstats")) {
+                try {
+                    getLogger().warning("--------------------[ READ ME PLEASE ]--------------------");
+                    getLogger().warning("This is your first startup with AntiPopup.");
+                    getLogger().warning("I would like to kindly ask you to enable bstats");
+                    getLogger().warning("configuration value to help me improve AntiPopup.");
+                    getLogger().warning("Because I respect your freedom it's disabled by default.");
+                    getLogger().warning("Thanks for using AntiPopup! (you will not see this again)");
+                    getLogger().warning("----------------------------------------------------------");
+                    config.set("ask-bstats", false);
+                    config.save();
+                } catch (IOException io) {
+                    io.printStackTrace();
+                }
+            }
+        }, 5);
     }
 
     @Override
