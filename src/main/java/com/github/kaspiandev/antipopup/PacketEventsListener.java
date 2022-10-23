2cd7dceed23a142c3ca1c6747a3ce6c74c7c5f67
Kaspian <kacperleaguenine@gmail.com>
2022-10-23 18:41:51 +0200

    🔼 5-RC2

--- src/main/java/com/github/kaspiandev/antipopup/PacketEventsListener.java
+++ src/main/java/com/github/kaspiandev/antipopup/PacketEventsListener.java
@@ -8,6 +8,8 @@ import com.github.retrooper.packetevents.protocol.chat.message.ChatMessage_v1_19
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerChatMessage;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerServerData;
+import com.github.retrooper.packetevents.wrapper.status.server.WrapperStatusServerResponse;
+import com.google.gson.JsonObject;
 
 import java.util.UUID;
 
@@ -16,8 +18,16 @@ public class PacketEventsListener extends PacketListenerAbstract {
     public PacketEventsListener() {
         super(PacketListenerPriority.HIGH);
     }
+
     @Override
     public void onPacketSend(PacketSendEvent event) {
+        if (event.getPacketType() == PacketType.Status.Server.RESPONSE) {
+            WrapperStatusServerResponse wrapper = new WrapperStatusServerResponse(event);
+            JsonObject newObj = wrapper.getComponent();
+            newObj.addProperty("preventsChatReports", true);
+            wrapper.setComponent(newObj);
+
+        }
         if (event.getPacketType() == PacketType.Play.Server.SERVER_DATA) {
             WrapperPlayServerServerData serverData = new WrapperPlayServerServerData(event);
             serverData.setEnforceSecureChat(true);
