a989e5dbfe5cae1e332021071999bacc3b6c8fb8
KaspianDev <kacperleaguenine@gmail.com>
2024-07-01 20:41:25 +0200

    Remove leftover testing code

--- shared/src/main/java/com/github/kaspiandev/antipopup/listener/PacketEventsListener.java
+++ shared/src/main/java/com/github/kaspiandev/antipopup/listener/PacketEventsListener.java
@@ -5,21 +5,13 @@ import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
-import com.github.retrooper.packetevents.protocol.chat.ChatType;
-import com.github.retrooper.packetevents.protocol.chat.ChatTypes;
-import com.github.retrooper.packetevents.protocol.chat.message.ChatMessage;
-import com.github.retrooper.packetevents.protocol.chat.message.ChatMessage_v1_19_1;
-import com.github.retrooper.packetevents.protocol.chat.message.ChatMessage_v1_19_3;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.packettype.PacketTypeCommon;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerChatMessage;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerJoinGame;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerServerData;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSystemChatMessage;
 import com.github.retrooper.packetevents.wrapper.status.server.WrapperStatusServerResponse;
 import com.google.gson.JsonObject;
-import net.kyori.adventure.text.Component;
 
 public class PacketEventsListener extends PacketListenerAbstract {
 
@@ -53,20 +45,6 @@ public class PacketEventsListener extends PacketListenerAbstract {
         } else if (packetType == PacketType.Play.Server.PLAYER_CHAT_HEADER
                 && !platform.getApConfig().isSendHeader()) {
             event.setCancelled(true);
-        } else if (platform.getApConfig().isExperimentalMode() && packetType == PacketType.Play.Server.CHAT_MESSAGE) {
-            WrapperPlayServerChatMessage wrapper = new WrapperPlayServerChatMessage(event);
-            ChatMessage chatMessage = wrapper.getMessage();
-            if (chatMessage instanceof ChatMessage_v1_19_1 message_v1_19_1) {
-                event.setCancelled(true);
-                ChatType.Bound formatting = message_v1_19_1.getChatFormatting();
-                Component component = message_v1_19_1.getType().getChatDecoration().decorate(message_v1_19_1.getChatContent(), formatting);
-                event.getUser().sendPacket(new WrapperPlayServerSystemChatMessage(false, component));
-            } else if (chatMessage instanceof ChatMessage_v1_19_3 message_v1_19_3) {
-                event.setCancelled(true);
-                ChatType.Bound formatting = message_v1_19_3.getChatFormatting();
-                Component component = message_v1_19_3.getType().getChatDecoration().decorate(message_v1_19_3.getChatContent(), formatting);
-                event.getUser().sendPacket(new WrapperPlayServerSystemChatMessage(false, component));
-            }
         }
     }
 
