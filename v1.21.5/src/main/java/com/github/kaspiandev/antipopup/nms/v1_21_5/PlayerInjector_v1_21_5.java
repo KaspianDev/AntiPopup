59c3502d32ae665d21cab96411285e61dd57d215
Kaspian <34658474+KaspianDev@users.noreply.github.com>
2025-04-20 17:44:21 +0200

    11 (#113)
    * 1.21.5 support work
    
    * Fix build?
    
    * Final fixes
    
    * Update release.yml

---
+++ v1.21.5/src/main/java/com/github/kaspiandev/antipopup/nms/v1_21_5/PlayerInjector_v1_21_5.java
@@ -0,0 +1,86 @@
+package com.github.kaspiandev.antipopup.nms.v1_21_5;
+
+import com.github.kaspiandev.antipopup.spigot.nms.PacketInjector;
+import io.netty.channel.*;
+import net.minecraft.network.Connection;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundPlayerChatPacket;
+import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
+import net.minecraft.server.network.ServerCommonPacketListenerImpl;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InaccessibleObjectException;
+
+@SuppressWarnings("unused")
+public class PlayerInjector_v1_21_5 implements PacketInjector {
+
+    private static final String HANDLER_NAME = "antipopup_handler";
+    private static Field connectionField;
+
+    static {
+        try {
+            for (Field field : ServerCommonPacketListenerImpl.class.getDeclaredFields()) {
+                if (field.getType().equals(Connection.class)) {
+                    field.setAccessible(true);
+                    connectionField = field;
+                    break;
+                }
+            }
+        } catch (SecurityException | InaccessibleObjectException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void inject(Player player) {
+        ChannelDuplexHandler duplexHandler = new ChannelDuplexHandler() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object packet, ChannelPromise promise) throws Exception {
+                if (packet instanceof ClientboundPlayerChatPacket chatPacket) {
+                    Component content = chatPacket.unsignedContent();
+                    if (content == null) {
+                        content = Component.literal(chatPacket.body().content());
+                    }
+                    ChatType.Bound chatType = chatPacket.chatType();
+
+                    ((CraftPlayer) player).getHandle().connection.send(
+                            new ClientboundSystemChatPacket(chatType.decorate(content), false));
+                    return;
+                }
+                super.write(ctx, packet, promise);
+            }
+        };
+
+        ServerGamePacketListenerImpl listener = ((CraftPlayer) player).getHandle().connection;
+        Channel channel = getConnection(listener).channel;
+        ChannelPipeline pipeline = channel.pipeline();
+
+        if (pipeline.get(HANDLER_NAME) != null) {
+            pipeline.remove(HANDLER_NAME);
+        }
+
+        channel.eventLoop().submit(() -> {
+            channel.pipeline().addBefore("packet_handler", HANDLER_NAME, duplexHandler);
+        });
+    }
+
+    public void uninject(Player player) {
+        ServerGamePacketListenerImpl listener = ((CraftPlayer) player).getHandle().connection;
+        Channel channel = getConnection(listener).channel;
+        channel.eventLoop().submit(() -> {
+            channel.pipeline().remove(HANDLER_NAME);
+        });
+    }
+
+    private Connection getConnection(ServerGamePacketListenerImpl listener) {
+        try {
+            return (Connection) connectionField.get(listener);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
